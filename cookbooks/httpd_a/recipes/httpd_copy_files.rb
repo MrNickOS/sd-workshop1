08518a06d1d547412ae102e158abfff68f861f61
MrNickOS <nicomac11@hotmail.com>
2017-09-11 19:26:46 -0500

    Update httpd_copy_files.rb

--- cookbooks/httpd_a/recipes/httpd_copy_files.rb
+++ cookbooks/httpd_a/recipes/httpd_copy_files.rb
@@ -18,17 +18,3 @@ cookbook_file '/var/www/html/info.php' do
 		owner 'root'
 		group 'wheel'
 end
-
-#courses = node[:courses]
-#node[courses].each do |course|
-#	puts "<h1>#{course}</h1>"
-
-#template '/var/www/html/courses.html' do
-#	source 'courses.erb'
-#	mode 0644
-#	owner 'root'
-#	group 'wheel'
-#	variables(
-#		:courses => node[:courses]
-#	)
-#end
